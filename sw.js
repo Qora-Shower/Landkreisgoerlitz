a08fda40f62ba88fb53cdc53fc5b63d95541ac7a
Qora <lorismziray01@gmail.com>
2025-08-09 15:16:04 +0200

    update

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "LGörlotz-V1";
+const BASE = "/Landkreisgoerlitz/";
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        
+        BASE + 'index.html',
+        BASE + 'index.css'
+        
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.open(CACHE_NAME).then(cache =>
+    cache.match(event.request).then(resp => {
+      return resp || fetch(event.request).then(fresp => {
+        cache.put(event.request, fresp.clone());
+        return fresp;
+      });
+    })
+  ));
+});
